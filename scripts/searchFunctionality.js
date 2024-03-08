76c47e7acf913c0f3414fc723380b03dd0ec74ff
luc perrot <luc.perrot@hotmail.fr>
2024-03-08 10:56:52 +0100

    Scénario alternatif A1 implemented

--- scripts/searchFunctionality.js
+++ scripts/searchFunctionality.js
@@ -3,6 +3,8 @@ import { populateFilters } from './filterTags.js';
 
 function addSearchFunctionality(recipes) {
   const searchBar = document.getElementById('searchBar');
+  const noResultsMessage = document.getElementById('no-results-message');
+
   searchBar.addEventListener('input', () => {
     const searchTerm = searchBar.value.trim().toLowerCase();
     if (searchTerm.length >= 3) {
@@ -14,15 +16,28 @@ function addSearchFunctionality(recipes) {
         recipe.description.toLowerCase().includes(searchTerm)
       );
       updateDisplayedRecipes(filteredRecipes);
+
+      if (filteredRecipes.length === 0) {
+        // Afficher le message d'erreur
+        noResultsMessage.textContent = `Aucune recette ne contient '${searchTerm}'. Vous pouvez chercher "tarte aux pommes", "poisson", etc.`;
+        noResultsMessage.style.display = 'block';
+      } else {
+        // Cacher le message d'erreur s'il y a des résultats
+        noResultsMessage.style.display = 'none';
+      }
+    } else {
+      // Cacher le message d'erreur si la longueur de la recherche est inférieure à 3 caractères
+      noResultsMessage.style.display = 'none';
     }
   });
 }
 
+
 function updateDisplayedRecipes(filteredRecipes, allRecipes) {
   const recipesContainer = document.getElementById('recipes-container');
-  recipesContainer.innerHTML = ''; // Clear previous content
+  recipesContainer.innerHTML = '';
   createRecipeCards(filteredRecipes);
-  populateFilters(allRecipes, filteredRecipes); // Update filters with filtered recipes
+  populateFilters(allRecipes, filteredRecipes);
 }
 
 export { addSearchFunctionality, updateDisplayedRecipes };
